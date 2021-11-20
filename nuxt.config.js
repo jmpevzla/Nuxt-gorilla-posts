e6dbd8d95aaa02cdeb5e4db70b254708be6c0036
Jose M Perez <jmperezvzla@gmail.com>
2021-11-19 21:50:09 -0400

    add layout for index

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    proxy:true,
+    proxy: true,
     credentials: false
   },
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,76 @@
 <template>
   <section class="container">
-    <h1>Gorilla News Posts</h1>
+    <h1 class="text-center">Gorilla News Posts</h1>
     <div>
-      <ul v-for="post in posts" :key="post.id">
-        <li>{{ post }}</li>
-      </ul>
+      <md-content
+        class="md-layout md-gutter"
+        style="background: #ccc; padding: 5px"
+      >
+        <ul
+          v-for="post in posts"
+          :key="post.id"
+          class="
+            md-layout-item
+            md-large-size-25
+            md-medium-size-33
+            md-small-size-50
+            md-xsmall-size-100
+          "
+        >
+          <md-card md-with-hover style="margin-top: 10px">
+            <md-card-media>
+              <img
+                :alt="post.title.rendered"
+                v-if="post._embedded"
+                :src="
+                  post._embedded['wp:featuredmedia'][0].media_details.sizes
+                    .medium_large.source_url
+                "
+              />
+            </md-card-media>
+
+            <md-card-header>
+              <div class="md-title">
+                <a
+                  target="_blank"
+                  :href="post.link"
+                  :title="post.title.rendered"
+                  >{{ post.title.rendered }}</a
+                >
+              </div>
+              <div class="md-subhead" v-if="post._embedded">
+                {{ post._embedded.author[0].name }}
+              </div>
+            </md-card-header>
+
+            <md-card-content>
+              <md-button class="md-icon-button">
+                <md-icon class="small-icon">bookmark</md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon class="small-icon">share</md-icon>
+              </md-button>
+            </md-card-content>
+          </md-card>
+        </ul>
+      </md-content>
     </div>
   </section>
 </template>
 
-<script>
-  export default {
-    async asyncData({ app }) {
-      const posts = await app.$axios.$get('/api/')
-      return { posts }
-    }
+<style lang="scss">
+  .text-center {
+    text-align: center;
   }
+</style>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const posts = await app.$axios.$get('/api/?_embed')
+    console.log(posts)
+    return { posts }
+  },
+}
 </script>
